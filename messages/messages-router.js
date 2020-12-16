9afbc4ae7eec7a5bb39b63ea72e39cfbb9648220
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-12-15 20:37:09 -0600

    Finished in class - coded along

--- messages/messages-router.js
+++ messages/messages-router.js
@@ -3,24 +3,66 @@ const db = require("../data/config")
 
 const router = express.Router()
 
-router.get("/", (req, res, next) => {
-
+router.get("/", async (req, res, next) => {
+    try{
+        //SELECT * FROM Messages;
+        const messages = await db.select("*").from("messages")
+        res.json(messages)
+    } catch(err) {
+        next(err)
+    }
 })
 
-router.get("/:id", (req, res, next) => {
-
+router.get("/:id", async (req, res, next) => {
+    try{
+        //SELECT * FROM Messages WHERE id = ?;
+        const [message] = await db.select("*").from("messages").where("id", req.params.id).limit(1)
+        res.json(message)
+    } catch(err) {
+        next(err)
+    }
 })
 
-router.post("/", (req, res, next) => {
-
+router.post("/", async (req, res, next) => {
+    try{
+    const payload = {
+        title: req.body.title,
+        contents: req.body.contents,
+        }
+        // INSERT INTO messages (title, contents) VALUES (?, ?);
+        const [id] = await db.insert(payload).into("messages")
+        const message = await db("messages").first().where("id", id)
+        res.status(201).json(message)
+    } catch(err) {
+        next(err)
+    }
 })
 
-router.put("/:id", (req, res, next) => {
-
+router.put("/:id", async (req, res, next) => {
+    try{
+        const payload = {
+            title: req.body.title,
+            contents: req.body.contents,
+            }
+        //UPDATE messages SET title = ? AND contents = ? WHERE id = ?;
+        await db("messages").where("id", req.params.id).update(payload)
+        const message = await db("messages").where("id", req.params.id).first()
+        res.json(message)
+    } catch(err) {
+        next(err)
+    }
 })
 
-router.delete("/:id", (req, res, next) => {
-
+router.delete("/:id", async (req, res, next) => {
+    try{
+        //DELETE FROM messages WHERE id = ?;
+        await db("messages").where("id", req.params.id).del()
+        //since we no longer have a resource to return, just send a 204
+        //which means success, but no response data is being sent back.
+        res.status(204).end()
+    } catch(err) {
+        next(err)
+    }
 })
 
 module.exports = router
